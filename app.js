f41f75f6119c87d65be0e4143fe8f13e142bb47b
gabrielmj23 <gabrielmj2312@gmail.com>
2022-03-08 16:55:11 -0400

    Final touches before deployment

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
+var helmet = require('helmet');
 
 // For authentication
 const session = require("express-session");
@@ -27,6 +29,8 @@ db.on('error', console.error.bind(console, 'mongo connection error'));
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(helmet());
 
 // Set up session middleware
 app.use(session({secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true}));
@@ -75,7 +79,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-// Set up routes
+// Get routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -83,8 +87,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set up routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
